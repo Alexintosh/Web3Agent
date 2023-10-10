dd3655e97b61c26a2239b21db545af4341ca642f
mayurbagga <mayurbagga@hotmail.com>
2023-10-10 17:58:19 +0530

    place some minor design changes

--- app/defi-prompt/page.tsx
+++ app/defi-prompt/page.tsx
@@ -11,13 +11,16 @@ export default function Page() {
 
     return (
       <>
-      
-        <div style={{display:"flex", flexDirection:"column", alignItems:"center",  padding:"20px", border:"10px",borderColor:"green", borderRadius:"10px"}} className='text-lg'>
-            <h1 style={{fontSize:"28px", fontWeight:"bold",marginTop:"5px"}} >Magic space</h1> 
+            <div className="rounded-lg border bg-background shadow-md">
+
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center",  padding:"20px", border:"10px",borderColor:"green", borderRadius:"10px"}} className='card rounded-none'>
+                    {/* <h1 style={ { fontSize: "28px", fontWeight: "bold", marginTop: "5px" } } >Magic space</h1>  */}
+                            <h1 className=" text-emerald-700 text-center text-3xl font-black">Magic space</h1> 
+
             <TabsRequest />
         </div>
         
-
+</div>
       </>
   );
 }
