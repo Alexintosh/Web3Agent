80b959664a8ef8ed39ce0cb7a2e45bff44ca2134
mayurbagga <mayurbagga@hotmail.com>
2023-10-10 14:10:56 +0530

    request form with tab added

--- app/defi-prompt/page.tsx
+++ app/defi-prompt/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+
+"use client"; // This is a client component 👈🏽
+
+import React from 'react';
+import TabsRequest  from '../components/ui/tabs-request'; // Import the Tabs component without curly braces
+
 
 export default function Page() {
+  
+
+
     return (
-        <div className='text-lg'>Lets build here</div>
-    )
+      <>
+      
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center",  padding:"20px", border:"10px",borderColor:"green", borderRadius:"10px"}} className='text-lg'>
+            <h1 style={{fontSize:"28px", fontWeight:"bold",marginTop:"5px"}} >Magic space</h1> 
+            <TabsRequest />
+        </div>
+        
+
+      </>
+  );
 }
